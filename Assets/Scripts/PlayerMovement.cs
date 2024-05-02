f250843ed57ab181ea8f76042c0b089eca0110c3
Ricardo Tanaka Terukina <rtanaka@talento.tls.edu.pe>
2024-05-02 17:20:24 -0500

    Added player shoot

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,20 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D rb2d;
+    private Vector2 direction;
+
+    public Vector2 Direction
+    {
+        get
+        {
+            return direction;
+        }
+    }
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        direction = Vector2.up;
     }
 
     void Update()
@@ -21,6 +31,11 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
+        if(horizontal != 0f || vertical != 0f)
+        {
+            direction = new Vector2(horizontal, vertical).normalized;
+        }
 
         rb2d.velocity = new Vector2(horizontal, vertical).normalized * speed;
     }
